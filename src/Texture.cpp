3cdfb2f91a3b075c7a2ca115409e2eea4cda3e11
IRCSS <sh.shahrabi@hotmail.com>
2019-12-13 23:40:03 +0100

    fixed the issue with stb_image.cpp not being compiled. It wasnt in the filters and the vcxproj didnt know about this file

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -1,5 +1,4 @@
 #include "Texture.h"
-#define STB_IMAGE_IMPLEMENTATION
 #include "vendor/stb_image/stb_image.h"
 
 Texture::Texture(const std::string& path):
--- src/vendor/stb_image/stb_image.cpp
+++ src/vendor/stb_image/stb_image.cpp
@@ -1,2 +1,6 @@
 #define STB_IMAGE_IMPLEMENTATION    
 #include "stb_image.h"
+
+int DoRandomThing(){
+	return 7;
+}
