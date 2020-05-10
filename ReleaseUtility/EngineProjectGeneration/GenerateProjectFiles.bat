cd4f904d5694fee87918f0e41266681427216d46
Sha <sh.shahrabi@hotmail.com>
2020-05-10 21:49:31 +0200

    made a pipeline to create new projects with the engine using premake. It generates visual studio 2017 files with all the configuration, macros and linking done. Plus coppies over  the standard SandBox.cpp so that after generating the files with the given name, you have a started scene you can compile and start off with

---
+++ ReleaseUtility/EngineProjectGeneration/GenerateProjectFiles.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /D "%~dp0"
+set /p ProjName="Enter the name of your project: "
+
+if not exist %ProjName%\\src MkDir %ProjName%\\src && copy "ToyRendererDistrubtionFiles\\SandBoxApp.cpp" "%ProjName%\\src\\SandBoxApp.cpp"
+call premake\premake5.exe vs2017 --GameName="%ProjName%"
+PAUSE
---
+++ ReleaseUtility/EngineProjectGeneration/premake5.lua
@@ -0,0 +1,71 @@
+newoption {
+  trigger     = "GameName",
+  value       = "NameOfTheGame",
+  description = "Give Your project a name",
+  default     = "ExampleGame",
+}
+
+if not _OPTIONS["GameName"] then
+  _OPTIONS["GameName"] = "ExampleGame"
+end
+workspace (_OPTIONS["GameName"])
+architecture "x64"
+configurations {
+    "Debug",
+    "Release",
+    "Shipping"
+ }
+ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+ project (_OPTIONS["GameName"])
+ location (_OPTIONS["GameName"])
+ kind "ConsoleApp"
+ language "C++"
+
+ targetdir("binaries/" ..outputdir.. "/%{prj.name}")
+ objdir("intermediates/" ..outputdir.. "/%{prj.name}")
+
+ files{
+     "%{prj.name}/src/**.h",
+     "%{prj.name}/src/**.cpp",
+  }
+
+  libdirs { 
+    "ToyRendererDistrubtionFiles",
+ }
+
+  includedirs{
+    "ToyRendererDistrubtionFiles/include"
+  }
+
+  links{
+      "ToyRenderer.lib"
+  }
+
+  filter "system:windows"
+  cppdialect "C++17"
+  staticruntime "On"
+  systemversion "latest"
+
+  defines{
+    "TOYRENDERER_PLATFORM_WINDOWS",
+    "_MBCS",
+    "GLEW_STATIC"
+   }
+
+   postbuildcommands{
+    ("copy ..\\ToyRendererDistrubtionFiles\\ToyRenderer.dll ..\\binaries\\" ..outputdir.. "\\%{prj.name}\\"),
+    ("xcopy ..\\ToyRendererDistrubtionFiles\\res ..\\binaries\\" ..outputdir.. "\\%{prj.name}\\res\\ /s /e")
+  }
+
+   filter "configurations:Debug"
+   defines "TOYRENDERER_DEBUG"
+   symbols "On"
+
+filter "configurations:Release"
+   defines "TOYRENDERER_RELEASE"
+   optimize "On"
+
+filter "configurations:Shipping"
+   defines "TOYRENDERER_SHIPPING"
+   optimize "On"
