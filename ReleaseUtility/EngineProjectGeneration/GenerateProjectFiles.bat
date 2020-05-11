54184bcc32514d3c1ed9e79675bae1f19fad129b
Sha <sh.shahrabi@hotmail.com>
2020-05-11 13:16:19 +0200

    changed the generate project files so that it overwrites the sandbox app cpp file if it already exists, this should make engine updates on the client side easier

--- ReleaseUtility/EngineProjectGeneration/GenerateProjectFiles.bat
+++ ReleaseUtility/EngineProjectGeneration/GenerateProjectFiles.bat
@@ -2,6 +2,8 @@
 cd /D "%~dp0"
 set /p ProjName="Enter the name of your project: "
 
-if not exist %ProjName%\\src MkDir %ProjName%\\src && copy "ToyRendererDistrubtionFiles\\SandBoxApp.cpp" "%ProjName%\\src\\SandBoxApp.cpp"
+if not exist %ProjName%\\src MkDir %ProjName%\\src
+copy "ToyRendererDistrubtionFiles\\SandBoxApp.cpp" "%ProjName%\\src\\SandBoxApp.cpp" /b/v/y
+
 call premake\premake5.exe vs2017 --GameName="%ProjName%"
 PAUSE
