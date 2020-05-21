7400966d31083e338e740a82b98094a501651998
Sha <sh.shahrabi@hotmail.com>
2020-05-21 22:20:54 +0200

    finished the implementation of the Engine Release packet generation bat file. The file now copies over all the nesscerly content to the specified path. Left is to figure out if there is a way to also build the stuff through visual studio in the bat file without cmake or too much stuff.
    Also renamed the engineprojection folder to clientProject because that is what those scripts
    actually do.

--- ReleaseUtility/EngineReleasePackageGeneration/GenerateReleasePackage.bat
+++ ReleaseUtility/EngineReleasePackageGeneration/GenerateReleasePackage.bat
@@ -17,7 +17,7 @@ xcopy /Q /E /Y /I  "%solutionPath%vendor\\bin\\premake" ToyRendererReleasePackag
 mkdir ToyRendererReleasePackage\\ToyRendererDistrubtionFiles
 
 rem Coppying the header files
-ROBOCOPY %solutionPath%ToyRenderer\\src\\ ToyRendererReleasePackage\\ToyRendererDistrubtionFiles\\include\\ *.h *.hpp *.inl /S
+ROBOCOPY %solutionPath%ToyRenderer\\src\\ ToyRendererReleasePackage\\include\\ *.h *.hpp *.inl /S
 
 rem copying the dll for debug, release 
 
@@ -32,6 +32,30 @@ if not exist %solutionPath%ToyRenderer-binaries\\Debug-windows-x86_64 echo "WARN
 echo "coppying over the ToyRenderer dll in Debug  mode from  %solutionPath%ToyRenderer-binaries\\Debug-windows-x86_64\\ToyRenderer ToyRendererReleasePackage\\ToyRendererDistrubtionFiles\\ToyRenderer-Debug"
 
 xcopy /Q /E /Y /I  %solutionPath%ToyRenderer-binaries\\Debug-windows-x86_64\\ToyRenderer ToyRendererReleasePackage\\ToyRendererDistrubtionFiles\\ToyRenderer-Debug
+
+rem Copying the res folder of the Engine 
+
+echo "coppying over the ToyRenderer res folder from %solutionPath%ToyRenderer\\res to ToyRendererReleasePackage\\ToyRendererDistrubtionFiles\\res\\engine"
+
+xcopy /Q /E /Y /I %solutionPath%ToyRenderer\\res ToyRendererReleasePackage\\ToyRendererDistrubtionFiles\\res\\engine
+
+rem Copying the res folder of sand box as starter content
+
+echo "coppying over the Sandbox res folder from  %solutionPath%example\\SandBox\\res to ToyRendererReleasePackage\\ToyRendererStarterContent\\res"
+
+xcopy /Q /E /Y /I %solutionPath%example\\SandBox\\res ToyRendererReleasePackage\\ToyRendererStarterContent\\res\\game
+
+rem Copying over the starter content CPP, aka the SandBox.CPP
+
+echo "coppying over the Sandbox.cpp from  %solutionPath%example\\SandBox\\src\\SandBoxApp.cpp to ToyRendererReleasePackage\\ToyRendererStarterContent\\SandBoxApp.cpp"
+
+xcopy /Q /E /Y /I %solutionPath%example\\SandBox\\src ToyRendererReleasePackage\\ToyRendererStarterContent
+
+rem Copying over the project generation files 
+
+echo "coppying over the project generation from  %solutionPath%ReleaseUtility\\ClientProjectGeneration to ToyRendererReleasePackage"
+
+xcopy /Q /E /Y /I %solutionPath%ReleaseUtility\\ClientProjectGeneration ToyRendererReleasePackage
 
 PAUSE
 
